1bc296e88cd3fdf23957efa3062f055afabf0316
Oliver Elder <oliver@wahanda.com>
2018-07-31 13:09:10 +0100

    Format JS files - spaces

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Redirect,
-  withRouter
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Redirect,
+    withRouter
 } from "react-router-dom";
 import LoginForm  from './login/Login.js';
 import logo from './logo.svg';
@@ -30,66 +30,66 @@ class Login extends React.Component {
         authentication.authenticate(this.state.username, this.state.password);
     }
     login = () => {
-    	authentication.authenticate(() => {
+        authentication.authenticate(() => {
             this.setState({ redirectToReferrer: true });
         });
     };
 
-  render() {
-//    <p>Please log in</p>
-//    <form onSubmit={this.handleSubmit}>
-//        <label>Email: <input type="text" name="email" /></label>
-//        <label>Password: <input type="password" name="password" /></label>
-//        <input type="submit" value="Submit" />
-//    </form>
-//    <button onClick={this.login}>Log in</button>
+    render() {
+// <p>Please log in</p>
+// <form onSubmit={this.handleSubmit}>
+// <label>Email: <input type="text" name="email" /></label>
+// <label>Password: <input type="password" name="password" /></label>
+// <input type="submit" value="Submit" />
+// </form>
+// <button onClick={this.login}>Log in</button>
 
-      return (
-      <div>
-        <LoginForm />
-      </div>
-    );
-  }
+        return (
+                <div>
+                <LoginForm />
+                </div>
+        );
+    }
 }
 
 const authentication = {
-  isAuthenticated: false,
-  authenticate(email, password) {
+        isAuthenticated: false,
+        authenticate(email, password) {
 
-    var params = {
-    	email: email,
-        password: password
-    }
+            var params = {
+                    email: email,
+                    password: password
+            }
 
-    const searchParams = Object.keys(params).map((key) => {
-        return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
-    }).join('&');
+            const searchParams = Object.keys(params).map((key) => {
+                return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+            }).join('&');
 
 
-    fetch('/api/login', {
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-        	'Accept': 'application/json',
+            fetch('/api/login', {
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+                    'Accept': 'application/json',
+                },
+                method: 'post',
+                body: searchParams
+            })
+            .then(response => Promise.all([response, response.json()]))
+            .then(([response, json]) => {
+                if(response.ok) {
+                    return json;
+                } else {
+                    throw new Error(json);
+                }
+            })
+            .catch(console.error);
+
+            this.isAuthenticated = true;
         },
-        method: 'post',
-        body: searchParams
-    })
-    .then(response => Promise.all([response, response.json()]))
-    .then(([response, json]) => {
-    	if(response.ok) {
-    		return json;
-    	} else {
-    		throw new Error(json);
-    	}
-    })
-    .catch(console.error);
-
-    this.isAuthenticated = true;
-  },
-  signout(cb) {
-    this.isAuthenticated = false;
-  }
+        signout(cb) {
+            this.isAuthenticated = false;
+        }
 };
 
 class App extends Component {
@@ -117,17 +117,17 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
+                <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
+                <img src={logo} className="App-logo" alt="logo"/>
                     <h1 className="App-title">{this.state.message}</h1>
-                </header>
-                <p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.....</p>
-	            <Router>
-	                <Route path="/login" component={Login} />
-	            </Router>
+                    </header>
+                    <p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.....</p>
+                    <Router>
+                    <Route path="/login" component={Login} />
+                    </Router>
 
-            </div>
+                    </div>
         );
     }
 }
